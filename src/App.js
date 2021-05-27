0ecc884b4bcbc640c85cc719fcd5bd322cf2c41f
Olex Filipenko <olex.filipenko.stratis@gmail.com>
2021-05-27 09:24:24 +0300

    [FEATURE] - JSX/JS - Add main components to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Typography, AppBar } from '@material-ui/core';
 
+import VideoPlayer from './components/VideoPlayer';
+import Notifications from './components/Notifications';
+import Options from './components/Options';
+
 function App() {
     return (
         <div>
             <AppBar position="static" color="inherit">
                     <Typography variant="h2" align="center">Video Chat</Typography>
             </AppBar>
-            {/* Video player */}
-            {/* Options -> Notifications */}
+            < VideoPlayer / >
+            <Options>
+                <Notifications />
+            </Options>
         </div>
     )
 }
