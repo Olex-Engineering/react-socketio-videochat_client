af5fcc37bcf0b309084814379798526d5c2cfb45
Olex Filipenko <olex.filipenko.stratis@gmail.com>
2021-05-28 11:04:20 +0300

    [BUGFIX] - JSX - fix VideoPlayer template

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -44,7 +44,7 @@ export const VideoPlayer = () => {
 
             {/* Target user video */}
             {
-                callAccepted && !callEnded (
+                callAccepted && !callEnded && (
                     <Paper className={classes.paper}>
                         <Grid item xs={12} md={6}>
                             <Typography variant="h5" gutterBottom>{ call.name || 'Name' }</Typography>
