d4feeaad8d49c6c8f6126dcaac7596e55fb8ee2a
Olex Filipenko <olex.filipenko.stratis@gmail.com>
2021-05-28 10:14:09 +0300

    [BUGFIX] - JSX - fix video player material styles import

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { Grid, Typography, Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { SocketContext } from '../SocketContext';
 
